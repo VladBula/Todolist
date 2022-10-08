1a3f2b6a533758bfb042997ce3ffe989a2bcb6c2
Vlad <vladbulynk@gmail.com>
2022-10-08 19:12:28 +0300

    api-key fix

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const settings = {
     withCredentials: true,
     headers: {
-        'API-KEY': '1cdd9f77-c60e-4af5-b194-659e4ebd5d41'
+        'API-KEY': 'bfa7db01-d291-4b43-9a1d-5cdebab39351'
     }
 }
 const instance = axios.create({
